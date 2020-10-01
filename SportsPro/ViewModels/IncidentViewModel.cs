d6185fbf73d98f3cd0725bc1394f84737e68d52e
huijillain <skyjillain@gmail.com>
2020-09-30 20:03:50 -0600

    Update Incident Delete.cshtml

--- SportsPro/ViewModels/IncidentViewModel.cs
+++ SportsPro/ViewModels/IncidentViewModel.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 
 namespace SportsPro.ViewModels
-{    public class IncidentViewModel
+{
+    public class IncidentViewModel
     {
         public List<Customer> Customers { get; set; }
         public string FilterString { get; set; }
         public string ActiveIncident { get; set; }
         public string ActiveTechnician { get; set; }
         public string Action { get; set; }
-
         public Incident CurrentIncident { get; set; }
 
         public List<Product> Products { get; set; }
--- SportsPro/ViewModels/TechIncidentViewModel.cs
+++ SportsPro/ViewModels/TechIncidentViewModel.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using SportsPro.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using SportsPro.Models;
 
 namespace SportsPro.ViewModels
 {
     public class TechIncidentViewModel
     {
         public Technician Technician { get; set; }
-        public List<Incident> Incidents { get; set; } 
+        public List<Incident> Incidents { get; set; }
     }
 }
