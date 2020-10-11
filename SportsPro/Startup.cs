ce514f8937c6897a67d5cfb254ffb060abc9d8b0
huijillain <skyjillain@gmail.com>
2020-10-10 20:26:36 -0600

    update on Stratup.cs

--- SportsPro/Startup.cs
+++ SportsPro/Startup.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
 using Microsoft.EntityFrameworkCore;
 using SportsPro.Models;
+using SportsPro.Models.DataLayer;
 using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
 
 namespace SportsPro
 {
@@ -22,29 +24,75 @@ namespace SportsPro
         // Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //adding Identity services
-            services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<SportsProContext>();
-
             //part5, Session state for Tech ID
             services.AddMemoryCache();
             services.AddSession();
-            services.AddControllersWithViews();
+
+            services.AddTransient(typeof(IUnitOfWork), typeof(UnitOfWork));
+            services.AddTransient(typeof(IGRepository<>), typeof(GRepository<>));
 
             // Unit testing
-            services.AddHttpContextAccessor();
+            //services.AddHttpContextAccessor();
 
             services.AddDbContext<SportsProContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("SportsProContext")));
 
-            services.AddRouting(options => {
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<SportsProContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+            });
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddRouting(options => 
+            {
                 options.LowercaseUrls = true;
                 options.AppendTrailingSlash = true;
             });
+        }
 
-            //services.AddTransient(typeof(IUnitOfWork), typeof(UnitOfWork));
-            //services.AddTransient(typeof(IGRepository<>), typeof(GRepository<>));
+        private async Task CreateUserRoles(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            IdentityResult roleResult;
+            //Adding Admin Role
+            var roleCheck = await RoleManager.RoleExistsAsync("Admin");
+            if (!roleCheck)
+            {
+                //create the roles and seed them to the database
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+
+            //Assign Admin role to the main User here we have given our newly registered 
+            //login id for Admin management
+            IdentityUser user = await UserManager.FindByEmailAsync("admin@sportsprosoftware.com");
+            var User = new IdentityUser();
+            await UserManager.AddToRoleAsync(user, "Admin");
+        }
+        private async Task CreateNewRoles(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            IdentityResult roleResult;
+            var roleCheck = await RoleManager.RoleExistsAsync("Tech");
+            if (!roleCheck)
+            {
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("Tech"));
+            }
+            IdentityUser user = await UserManager.FindByNameAsync("tech@sportsprosoftware.com");
+            var User = new IdentityUser();
+            await UserManager.AddToRoleAsync(user, "Tech");
         }
 
         // Use this method to configure the HTTP request pipeline.
@@ -65,10 +113,6 @@ namespace SportsPro
             app.UseRouting();
             app.UseAuthorization();
             app.UseAuthentication();
-            //app.UseMvc(routes =>
-            //{
-            //    routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
-            //});
 
             app.UseSession();  // must be called before UseEndpoints()
             app.UseEndpoints(endpoints =>
@@ -76,7 +120,10 @@ namespace SportsPro
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
+            CreateUserRoles(services).Wait();
+            CreateNewRoles(services).Wait();
         }
     }
 }
